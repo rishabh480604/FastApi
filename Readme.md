275fa8452f68491b2e37c328b55e99dce1f326ac
rishabh480604 <rishabh480604@gmail.com>
2024-09-29 01:13:49 +0530

    readme update

---
+++ Readme.md
@@ -0,0 +1,7 @@
+DB atlas data base is not set
+to setup create own cluster have database notes and folder notes
+
+and data having
+ desc:  <string>
+ title: <string>
+ important: <boolean>
--- config/db.py
+++ config/db.py
@@ -1,7 +1,7 @@
 from pymongo import MongoClient
 import certifi
-# DB is configured with notes have notes folder
-MONGO_URI = "mongodb+srv://rishabh22356:asdfghjkl@cluster0.ymgsy.mongodb.net"
+# DB is configured with notes have notes folder use own id and password
+MONGO_URI = "mongodb+srv://rishabh22356:<password>@cluster0.ymgsy.mongodb.net"
 
 conn=MongoClient(MONGO_URI,
                   tls=True,
